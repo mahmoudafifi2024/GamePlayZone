dfb8e6fba5f8a900d6334e8b73d2fb86830fae81
Mahmoud Afifi <mahmoudafifi2024@gmail.com>
2025-05-16 23:35:40 +0300

    fix some issues in Slider with Swiper js

--- frontend/src/Components/Home/Slider.tsx
+++ frontend/src/Components/Home/Slider.tsx
@@ -1,10 +1,11 @@
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 // import required modules
-import { Pagination } from 'swiper/modules';
+import { Pagination, Navigation, Autoplay } from 'swiper/modules';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import '../../Styles/Components/Home/Slider.css';
 
 import Swiper1 from '../../images/swiper1.jpg';
@@ -14,7 +15,14 @@ import Swiper4 from '../../images/swiper4.jpg';
 
 export default function Slider() {
 return (
-    <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+    <Swiper
+  pagination={{ clickable: true }}
+  navigation={true}
+  autoplay={{ delay: 3000, disableOnInteraction: false }}
+  loop={true}
+  modules={[Pagination, Navigation, Autoplay]}
+  className="mySwiper"
+>
     <SwiperSlide><img src={Swiper1} alt="Slide 1" /></SwiperSlide>
     <SwiperSlide><img src={Swiper2} alt="Slide 2" /></SwiperSlide>
     <SwiperSlide><img src={Swiper3} alt="Slide 3" /></SwiperSlide>
