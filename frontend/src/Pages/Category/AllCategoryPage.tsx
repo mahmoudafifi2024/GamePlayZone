00fa1b5072c9f9e2614cf2c517ba9afcc4d4d42d
Mahmoud Afifi <mahmoudafifi2024@gmail.com>
2025-06-21 12:34:19 +0300

    create DB , Backend structure

--- frontend/src/Pages/Category/AllCategoryPage.tsx
+++ frontend/src/Pages/Category/AllCategoryPage.tsx
@@ -2,6 +2,8 @@ import CategoryContainer from "../../Components/Category/CategoryContainer";
 import Pagination from "../../Components/Utility/Pagination";
 
 export default function AllCategoryPage() {
+
+    
 return (
     <div>
     <CategoryContainer />
